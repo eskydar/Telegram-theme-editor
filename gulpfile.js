67d6c2d9d0b460e92082fcbcf6e444d8f2fa6173
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2017-01-13 18:08:57 +0100

    initial commit with basic setup

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    stylish = require('jshint-stylish'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    minifycss = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('images', function(){
+  gulp.src('assets/src/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('assets/img/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['assets/css/src/base.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('jshint', function() {
+    return gulp.src('assets/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', ['browser-sync'], function(){
+  gulp.watch("assets/css/src/**/*.scss", ['styles']);
+  gulp.watch('assets/js/**/*.js', ['jshint']);
+  gulp.watch("*.html", ['bs-reload']);
+});
