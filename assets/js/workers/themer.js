bea2746fb5035eb26f3b6db0bf2057e939476963
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2017-01-15 20:56:23 +0100

    Temp solution for hover changes, will create css module that handles this

--- assets/js/workers/themer.js
+++ assets/js/workers/themer.js
@@ -8,7 +8,9 @@ define([
     Theme
 ){
     function setupBasicTheme (){
-        Theme.setDeep( 'config.titleBg', {element: 'titleBar', type: 'backgroundColor', color: ''} );
+        Theme.setDeep( 'config.titleBg', {element: 'titleBar', type: 'backgroundColor', color: '', pseudo: ''} );
+        Theme.setDeep( 'config.titleButtonCloseBgOver', {element: 'titleBar', type: 'backgroundColor', color: '', pseudo: 'hover'} );
+        Theme.setDeep( 'config.titleButtonCloseFgOver', {element: 'titleBar', type: 'backgroundColor', color: '', pseudo: 'hover'} );
     }
 
     function initialize () {
@@ -28,7 +30,22 @@ define([
     function patchElementStyle ( themeProperty, color ) {
         var data = Theme.getDeep( 'config.'+themeProperty );
         var element = $('[data-themeConfig-connection=' + data.element + ']');
-        element.css( U.camelCaseToDashed( data.type ), color );
+        // element.css( U.camelCaseToDashed( data.type ), color );
+
+        // Create the <style> tag
+        var style = document.createElement("style");
+
+        // Add a media (and/or media query) here if you'd like!
+        // style.setAttribute("media", "screen")
+        // style.setAttribute("media", "only screen and (max-width : 1024px)")
+
+        // WebKit hack :(
+        style.appendChild(document.createTextNode(""));
+
+        // Add the <style> element to the page
+        document.head.appendChild(style);
+        style.sheet.insertRule('[data-themeConfig-connection="' + data.element + '"]:hover { background: green !important; }', 0);
+        console.log(document.styleSheets)
     }
 
     return {
