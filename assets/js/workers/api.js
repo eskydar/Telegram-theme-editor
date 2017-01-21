73aecf9a33afd474d3c8d6117be3ccaf28e68fa1
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2017-01-21 20:40:17 +0100

    added the api request handler and styled the chat bar, need to style chat window, connect all items and style the page itself

---
+++ assets/js/workers/api.js
@@ -0,0 +1,37 @@
+define([
+    'underscore',
+    'helpers/utils'
+], function(
+    _,
+    U
+){
+    function request ( url, callback, method, data ) {
+        method = method || 'GET';
+        data = JSON.stringify(data) || null;
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 ) {
+                callback( {statusCode: xmlHttp.status, response: xmlHttp.responseText} );
+            }
+
+            xmlHttp.open(method, url, true);
+            xmlHttp.send(data);
+        }
+    }
+
+    function post ( url, callback, data ) {
+        request( url, callback, 'POST', data);
+    }
+
+    function get ( url, callback) {
+        request( url, callback, 'GET' );
+    }
+
+
+    return {
+        post: post,
+        get: get
+    };
+
+});
+
